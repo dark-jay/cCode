48ada9b700f4fb63384b75d0ad8ce394aa8b3246
Jayanta Nath <jay14nath@gmail.com>
2020-08-02 18:47:06 +0530

    resize split panel added

---
+++ renderer/js/resize.js
@@ -0,0 +1,9 @@
+ $(".editor-left").resizable({
+   handleSelector: ".splitter",
+   resizeHeight: false
+ });
+
+ $(".editor-input").resizable({
+   handleSelector: ".splitter-horizontal",
+   resizeWidth: false
+ });
